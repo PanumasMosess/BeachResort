4418b594e0968a55c88cae2fb101255056e4b390
Panumas Srisook <Panumas.srisook@gmail.com>
2019-06-05 07:01:19 +0700

    Feature Room Cretae

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+import FeturedRooms from "../components/FeturedRooms";
 import { Link } from "react-router-dom";
-import Services from '../components/Services';
+import Services from "../components/Services";
 
 export default function Home() {
   return (
     <>
-    <Hero>
-      <Banner title="luxurious room" subtitle="deluxe rooms starting at $299" >
-        <Link to='/rooms' className="btn-primary">our rooms</Link>
-      </Banner>
-    </Hero>
-    <Services />
+      <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeturedRooms />
     </>
   );
 }
